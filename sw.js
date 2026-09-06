ad3784d2ff1f0de0afdfef18d9af12d32c490161
Andre Reu <andrereu@gmail.com>
2026-09-05 23:57:05 -0300

    chore: publica

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // fetch: cache-first para estático mesmo-origem; rede direta para /api/.
 // activate: apaga versões antigas do cache.
 
-const VERSAO = 'mistura-v12';
+const VERSAO = 'mistura-v13';
 
 const PRECACHE = [
   './',
